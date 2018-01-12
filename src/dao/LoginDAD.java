cac5b3e815c098697e0b3b4d85592e65b1ca0ac8
starrrr962 <ssbd123@hanmail.net>
2018-01-12 17:00:36 +0900

    cookie_login_mvc2

---
+++ src/dao/LoginDAD.java
@@ -0,0 +1,66 @@
+package dao;
+
+import java.sql.*;
+
+import com.sun.xml.internal.ws.Closeable;
+
+import vo.Member;
+import static db.JdbcUtil.*;
+
+public class LoginDAD {
+	private static LoginDAD loginDAO;
+	private Connection con;
+	
+	private LoginDAD() {
+		
+	}
+	
+	public static LoginDAD getInstande() {
+		if(loginDAO == null) {
+			loginDAO = new LoginDAD();
+		}
+		return loginDAO;
+	}
+	
+	public void setConnection(Connection con) {
+		this.con = con;
+	}
+	
+	public Member selectLoginMember(String id, String passwd) {
+		Member loginMember = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = con.prepareStatement("select * from users where id = ? and passwd = ?");
+			pstmt.setString(1, id);
+			pstmt.setString(2, passwd);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				loginMember = new Member();
+				loginMember.setAddr(rs.getString("addr"));
+				loginMember.setAge(rs.getInt("age"));
+				loginMember.setEmail(rs.getString("email"));
+				loginMember.setGender(rs.getString("gender"));
+				loginMember.setId(rs.getString("id"));
+				loginMember.setName(rs.getString("name"));
+				loginMember.setNation(rs.getString("nation"));
+				loginMember.setPasswd(rs.getString("passwd"));
+				loginMember.setGrade(rs.getString("grade"));
+			}
+		}catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}finally {
+			try {
+				close(rs);
+				close(pstmt);
+			}catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		return loginMember;
+	}
+	
+
+}
